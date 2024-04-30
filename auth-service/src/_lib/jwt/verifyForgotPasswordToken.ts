7a86914ef043ca6d85e7dc6585eaa823b7084f25
Rithick <rithick.panoor574@gmail.com>
2024-04-30 20:07:50 +0530

    fix:bugs in reset password

--- auth-service/src/_lib/jwt/verifyForgotPasswordToken.ts
+++ auth-service/src/_lib/jwt/verifyForgotPasswordToken.ts
@@ -1,17 +1,18 @@
 import jwt from "jsonwebtoken";
 
-export const verifyForgetPasswordToken = (token: string) => {
-    return jwt.verify(
-      token,
-      String(process.env.FORGOT_PASSWORD_TOKEN_SECRET),
-      (error, decoded) => {
-        if (error) {
-          throw new Error(error?.message);
-        } else {
-          console.log("🚀 ~ file: verifyForgotPasswordToken.ts:13 ~ verifyForgetPasswordToken ~ decoded:", decoded)
-          return decoded;
-        }
-      }
-    );
-  };
-  
+export const verifyForgetPasswordToken = (token: string): Promise<any> => {
+    return new Promise((resolve, reject) => {
+        jwt.verify(
+            token,
+            String(process.env.FORGOT_PASSWORD_TOKEN_SECRET),
+            (error, decoded) => {
+                if (error) {
+                    reject(new Error(error.message));
+                } else {
+                    console.log("🚀 ~ file: verifyForgotPasswordToken.ts ~ decoded:", decoded);
+                    resolve(decoded);
+                }
+            }
+        );
+    });
+};
