75ec7ab19ce4fbef4048b7a4ca6356f58b70e7f6
Rithick <rithick.panoor574@gmail.com>
2024-05-03 10:38:24 +0530

    fix:kakfa consumer error fixed

--- notification-service/src/__boot/consumer.ts
+++ notification-service/src/__boot/consumer.ts
@@ -1,38 +1,53 @@
 import { consumer } from "@/infrastructure/kafka";
-import {INotificationSubscriber,createSubscriber} from "@/infrastructure/kafka"
-
+import { INotificationSubscriber, createSubscriber } from "@/infrastructure/kafka";
 
 export const runConsumer = async () => {
     try {
-
         await consumer.connect();
-
-        await consumer.subscribe({
-            topic: "notification-service-topic",
-            fromBeginning: true
-        });
-
+        await consumer.subscribe({ topic: "notification-service-topic", fromBeginning: true });
+        
         const subscriber = createSubscriber();
 
         await consumer.run({
             eachMessage: async ({ message }) => {
-
                 const { key, value } = message;
 
-                const subscriberMethod = String(key) as keyof INotificationSubscriber;
-                const subscriberData = JSON.parse(String(value));
+                if (!key) {
+                    console.error("Message key is missing.");
+                    return;
+                }
+                if (!value) {
+                    console.error("Message value is missing.");
+                    return;
+                }
+
+                const subscriberMethod = convertKeyToMethodName(String(key));;
+
+                if (typeof subscriber[subscriberMethod] !== 'function') {
+                    console.error(`Method ${subscriberMethod} is not defined on subscriber.`);
+                    return;
+                }
 
                 try {
+                    const subscriberData = JSON.parse(String(value));
                     await subscriber[subscriberMethod](subscriberData);
                 } catch (error: any) {
-                    throw new Error(error?.message);
+                    console.error(`Error processing message with key ${key}: ${error.message}`);
                 }
             }
         });
 
     } catch (error: any) {
-        throw new Error("Kafka Consume Error -> Notification : " + error?.message);
+        console.error("Kafka Consume Error -> Notification : ", error.message);
     }
+}
+
+function convertKeyToMethodName(key: string): keyof INotificationSubscriber {
+    const keyMap: { [key: string]: keyof INotificationSubscriber } = {
+        USER_CREATED_MESSAGE: 'userCreated',
+        REQUEST_FORGOT_PASSWORD_MESSAGE: 'requestForgotPassword'
+    };
+    return keyMap[key] || key;
 }
 
 export const stopConsumer = async () => {
