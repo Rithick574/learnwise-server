3c1cc3aaee03ff32c378269a5bac9bfeae4b676b
Rithick <rithick.panoor574@gmail.com>
2024-05-08 11:57:09 +0530

    feat:instructor application accepted mail added

--- notification-service/src/infrastructure/kafka/consumers/index.ts
+++ notification-service/src/infrastructure/kafka/consumers/index.ts
@@ -1,7 +1,9 @@
 import userCreatedConsumer from "./userCreatedConsumer";
 import requestForgotPasswordConsumer from "./requestForgotPasswordConsumer";
+import instructorEmailconfirmation from "./instructorEmailconfirmation"
 
 export {
     userCreatedConsumer,
-    requestForgotPasswordConsumer
+    requestForgotPasswordConsumer,
+    instructorEmailconfirmation
 }
--- notification-service/src/infrastructure/kafka/subscriber.ts
+++ notification-service/src/infrastructure/kafka/subscriber.ts
@@ -1,18 +1,21 @@
 import {
     userCreatedConsumer,
-    requestForgotPasswordConsumer
+    requestForgotPasswordConsumer,
+    instructorEmailconfirmation
 } from "./consumers";
 
 interface IUserEvents {
     userCreated(data: any): Promise<void>;
     requestForgotPassword(data:any):Promise<void>;
+    emailNotificationForInstructorApplication(data:any):Promise<void>;
 }
 
-export interface INotificationSubscriber extends Pick<IUserEvents, 'userCreated'  | 'requestForgotPassword'> { }
+export interface INotificationSubscriber extends Pick<IUserEvents, 'userCreated'  | 'requestForgotPassword' | 'emailNotificationForInstructorApplication'> { }
 
 export const createSubscriber = (): INotificationSubscriber => {
     return {
         userCreated: userCreatedConsumer,
         requestForgotPassword: requestForgotPasswordConsumer,
+        emailNotificationForInstructorApplication: instructorEmailconfirmation,
     }
 }
