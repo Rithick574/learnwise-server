97d5d499d26a03d818b37b65fa6e8bbae378c2d0
Rithick <rithick.panoor574@gmail.com>
2024-05-18 00:54:21 +0530

    shell command added to run services concurrently

---
+++ start-all.ps1
@@ -0,0 +1,4 @@
+pnpm concurrently "pnpm --prefix auth-service run start:dev" `
+                  "pnpm --prefix gateway-dev run start:dev" `
+                  "pnpm --prefix notification-service run start:dev" `
+                  "npm --prefix user-service run start:dev"
