4a98f4bfcc4dceeeb7d76ca5a657c7668cf79b71
Rithick <rithick.panoor574@gmail.com>
2024-05-20 21:45:58 +0530

    feat(shell):#course-server run command added

--- start-all.ps1
+++ start-all.ps1
@@ -1,4 +1,5 @@
 pnpm concurrently "pnpm --prefix auth-service run start:dev" `
                   "pnpm --prefix gateway-dev run start:dev" `
                   "pnpm --prefix notification-service run start:dev" `
-                  "npm --prefix user-service run start:dev"
+                  "npm --prefix user-service run start:dev" `
+                  "pnpm --prefix course-service run start:dev"
