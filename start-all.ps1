6b2b8a15879b0516709d0f3ed9423fd3587c0266
Rithick <rithick.panoor574@gmail.com>
2024-06-04 11:54:19 +0530

    chore:payment-service ps code added

--- start-all.ps1
+++ start-all.ps1
@@ -2,4 +2,5 @@ pnpm concurrently "pnpm --prefix auth-service run start:dev" `
                   "pnpm --prefix gateway-dev run start:dev" `
                   "pnpm --prefix notification-service run start:dev" `
                   "npm --prefix user-service run start:dev" `
-                  "pnpm --prefix course-service run start:dev"
+                  "pnpm --prefix course-service run start:dev" `
+                  "pnpm --prefix payment-service run start:dev"
